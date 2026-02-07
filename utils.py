9b5cf64c588908d654d60f9f8f6ae1231ca48da1
Saurya <sauryarajdav@gmail.com>
2026-02-07 23:11:40 +0530

    changes in utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import os
 
-from langchain_community.document_loaders import TextLoader
-from langchain_community.text_splitter import RecursiveCharacterTextSplitter
+import os
 
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
 
 
 # ---------------------------------------
